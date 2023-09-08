5c1caebd5f9755d080ddb4d71ee7cd463c35dd01
Denys <denys.hudymov@gmail.com>
2023-09-08 23:28:13 +0300

    unused imports was removed

--- lab1/src/main/java/org/denys/hudymov/AgreePrivacy.java
+++ lab1/src/main/java/org/denys/hudymov/AgreePrivacy.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static java.lang.Thread.sleep;
 
--- lab1/src/main/java/org/denys/hudymov/DemoWebShopTest.java
+++ lab1/src/main/java/org/denys/hudymov/DemoWebShopTest.java
@@ -7,10 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.Objects;
 
 @Getter
