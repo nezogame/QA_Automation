debc502bf1b1ddc74dbd65e28c90d89582ccfe47
Denys <denys.hudymov@gmail.com>
2023-09-08 23:21:44 +0300

    main class runs auto-testing

---
+++ lab1/src/main/java/org/denys/hudymov/App.java
@@ -0,0 +1,13 @@
+package org.denys.hudymov;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //PastebinAutoTest pastebinAutoTest= new PastebinAutoTest();
+        DemoWebShopTest demoWebShopTest= new DemoWebShopTest();
+        //pastebinAutoTest.startTest();
+        demoWebShopTest.startTest();
+    }
+}
