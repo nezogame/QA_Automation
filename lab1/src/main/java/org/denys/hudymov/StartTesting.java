77798e43025694c2143ba99b54b3eefd9d77533a
Denys <denys.hudymov@gmail.com>
2023-09-09 14:18:23 +0300

    The first part of the task was uncommented

--- lab1/src/main/java/org/denys/hudymov/StartTesting.java
+++ lab1/src/main/java/org/denys/hudymov/StartTesting.java
@@ -1,13 +1,13 @@
 package org.denys.hudymov;
 
 
-public class App 
+public class StartTesting
 {
     public static void main( String[] args )
     {
-        //PastebinAutoTest pastebinAutoTest= new PastebinAutoTest();
+        PastebinAutoTest pastebinAutoTest= new PastebinAutoTest();
         DemoWebShopTest demoWebShopTest= new DemoWebShopTest();
-        //pastebinAutoTest.startTest();
+        pastebinAutoTest.startTest();
         demoWebShopTest.startTest();
     }
 }
